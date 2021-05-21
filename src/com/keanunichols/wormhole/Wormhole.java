649cf7ef8b264b2bd7c18933de1865225926953f
Keanu Nichols <nichols.keanu9@gmail.com>
2021-05-21 08:45:57 -0400

    Add files via upload

--- src/com/keanunichols/wormhole/Wormhole.java
+++ src/com/keanunichols/wormhole/Wormhole.java
@@ -6,6 +6,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 //import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
@@ -29,8 +30,46 @@ public class Wormhole{
 		return number;
 	}
 	
+	protected void spawnCreeper(Location loc, World wld){
+		wld.spawnEntity(loc, EntityType.ENDERMAN);
+		wld.spawnEntity(loc, EntityType.GHAST);
+		wld.spawnEntity(loc, EntityType.WITHER_SKELETON);
+	}
 	
+	protected void pyramid(Location loc, Material mat, int r) {
+		Material[] wormholeBlockTypes = new Material[] {Material.GREEN_STAINED_GLASS,Material.RED_STAINED_GLASS};
+	    int px = loc.getBlockX();
+	    int py = loc.getBlockY();
+	    int pz = loc.getBlockZ();
+	    World w = loc.getWorld();
+	    /*
+	    int upper_x = px+2;
+	    int upper_z = pz+2;
+	    int lower_x = px-2;
+	    int lower_z = pz-2;
+	    */
+		for(int x=px-1; x<=px+1; x++){
+			for(int z=pz-1; z<=pz+1; z++){
+				if(x == px && z==pz){
+					continue;
+				}
+				w.getBlockAt(x, py, z).setType(wormholeBlockTypes[1]);
+			}
+		}
+
+		    
+	    /*
+	    for (int x = cx - r; x <= cx +r; x++) {
+	        for (int z = cz - r; z <= cz +r; z++) {
+	            if ((cx - x) * (cx - x) + (cz - z) * (cz - z) <= rSquared) {
+	                w.getBlockAt(x, cy, z).setType(mat);
+	            }
+	        }
+	    }*/
+	}
 	
+	//for five height create the glass (3,5,7,9,11)(top to bottom)
+	//                                 (green,cyan,blue,black,red)
 	protected Location generateWormhole(Player plr){
 		plr.sendMessage("Spawned block");
 		Location pLocation = plr.getLocation();
@@ -40,10 +79,11 @@ public class Wormhole{
 			nLocation.getBlock().setType(Material.AIR);
 		}
 		System.out.println("spawned block\n\n");
-		pLocation = pLocation.add(0,1,0);
-		pLocation.getBlock().setType(Material.OBSIDIAN);
-		moveToward(plr,pLocation,5);
+		pyramid(pLocation,Material.OBSIDIAN,2);
+		//pLocation = pLocation.add(0,1,0);
 		World wld = plr.getWorld();
+		moveToward(plr,pLocation,5);
+		spawnCreeper(pLocation,wld);
 		int randomNum = randomInt(15,40);
 		Location loc = new Location(wld, plr.getLocation().getBlockX(), plr.getLocation().getBlockY() + randomNum, plr.getLocation().getBlockZ());
 		return loc;
